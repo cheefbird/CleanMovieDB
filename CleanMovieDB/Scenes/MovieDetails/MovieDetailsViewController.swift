809b0162f6b91c5743762dbd6d8df97369f6f441
Francis Breidenbach <fbreidenbach@gmail.com>
2017-11-06 18:22:10 -0800

    Complete first draft of UI. Cleanup in viewController.

--- CleanMovieDB/Scenes/MovieDetails/MovieDetailsViewController.swift
+++ CleanMovieDB/Scenes/MovieDetails/MovieDetailsViewController.swift
@@ -12,34 +12,29 @@
 
 import UIKit
 
-protocol MovieDetailsDisplayLogic: class
-{
+protocol MovieDetailsDisplayLogic: class {
   func displaySomething(viewModel: MovieDetails.Something.ViewModel)
 }
 
-class MovieDetailsViewController: UIViewController, MovieDetailsDisplayLogic
-{
+class MovieDetailsViewController: UIViewController, MovieDetailsDisplayLogic {
   var interactor: MovieDetailsBusinessLogic?
   var router: (NSObjectProtocol & MovieDetailsRoutingLogic & MovieDetailsDataPassing)?
 
   // MARK: Object lifecycle
   
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
-  {
+  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     setup()
   }
   
-  required init?(coder aDecoder: NSCoder)
-  {
+  required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     setup()
   }
   
   // MARK: Setup
   
-  private func setup()
-  {
+  private func setup() {
     let viewController = self
     let interactor = MovieDetailsInteractor()
     let presenter = MovieDetailsPresenter()
@@ -54,8 +49,7 @@ class MovieDetailsViewController: UIViewController, MovieDetailsDisplayLogic
   
   // MARK: Routing
   
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-  {
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if let scene = segue.identifier {
       let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
       if let router = router, router.responds(to: selector) {
@@ -66,8 +60,7 @@ class MovieDetailsViewController: UIViewController, MovieDetailsDisplayLogic
   
   // MARK: View lifecycle
   
-  override func viewDidLoad()
-  {
+  override func viewDidLoad() {
     super.viewDidLoad()
     doSomething()
   }
@@ -76,14 +69,23 @@ class MovieDetailsViewController: UIViewController, MovieDetailsDisplayLogic
   
   //@IBOutlet weak var nameTextField: UITextField!
   
-  func doSomething()
-  {
+  func doSomething() {
     let request = MovieDetails.Something.Request()
     interactor?.doSomething(request: request)
   }
   
-  func displaySomething(viewModel: MovieDetails.Something.ViewModel)
-  {
+  func displaySomething(viewModel: MovieDetails.Something.ViewModel) {
     //nameTextField.text = viewModel.name
   }
 }
+
+
+
+
+
+
+
+
+
+
+
