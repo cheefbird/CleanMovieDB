364831ca2dd7537184d138d9b23e2c6bdf62aa66
Francis Breidenbach <fbreidenbach@gmail.com>
2017-11-10 19:15:47 -0800

    Create ReviewObject and subclasses. Reorganize Models in project navigator.

---
+++ CleanMovieDB/Models/Review/RealmReview.swift
@@ -0,0 +1,37 @@
+//
+//  RealmReview.swift
+//  CleanMovieDB
+//
+//  Created by Francis Breidenbach on 11/10/17.
+//  Copyright © 2017 Francis Breidenbach. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmReview: Object, ReviewObject {
+    
+    // MARK: - Persisted Properties
+    
+    @objc dynamic var id = ""
+    @objc dynamic var author = ""
+    @objc dynamic var content = ""
+    
+    // MARK: - Init
+    
+    convenience required init(copyFrom review: ReviewObject) {
+        self.init()
+        
+        id = review.id
+        author = review.author
+        content = review.content
+        movieID = review.movieID
+    }
+    
+    // MARK: - Overrides
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+    
+}
