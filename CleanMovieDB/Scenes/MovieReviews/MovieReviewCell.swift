ca04ec7e11ab98deccfb1fe5248ae15a8ecd11c3
Francis Breidenbach <francisbreidenbach@lax-breidenbach.fios-router.home>
2017-11-16 20:06:01 -0800

    Create MovieReviewCell. Build UI for cell.

---
+++ CleanMovieDB/Scenes/MovieReviews/MovieReviewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieReviewCell.swift
+//  CleanMovieDB
+//
+//  Created by Francis Breidenbach on 11/16/17.
+//  Copyright © 2017 Francis Breidenbach. All rights reserved.
+//
+
+import UIKit
+
+class MovieReviewCell: UITableViewCell {
+  
+  // MARK: - Outlets
+  
+  @IBOutlet var authorNameLabel: UILabel!
+  @IBOutlet var reviewContentLabel: UILabel!
+  
+  // MARK: - Methods
+
+}
